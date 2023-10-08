4df0ac7e362093d3b86b2331848142a2adbd879d
Renzo Medina <medinast30@gmail.com>
2023-10-08 20:37:52 -0300

    Add struct invoice, invoiceheader

--- pkg/invoiceheader/invoiceheader.go
+++ pkg/invoiceheader/invoiceheader.go
@@ -9,3 +9,26 @@ type Model struct {
 	CreateAt time.Time
 	UpdateAt time.Time
 }
+
+//slice of model
+type Models []*Model
+
+//Storage interface
+type Storage interface {
+	Migrate() error
+}
+
+//Service of product
+type Service struct {
+	storage Storage
+}
+
+//NewService return a pointer of Service
+func NewService(s Storage) *Service {
+	return &Service{s}
+}
+
+//Migrate use for migrate product
+func (s *Service) Migrate() error {
+	return s.storage.Migrate()
+}
